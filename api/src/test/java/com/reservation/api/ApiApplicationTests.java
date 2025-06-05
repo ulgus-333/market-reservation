fe2d776be0b0157ca36356a8952b4157f6f6f062
JIHYEON <jhkim@aptner.com>
2025-06-05 12:40:34 +0900

    chore: setting api module

---
+++ api/src/test/java/com/reservation/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservation.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
