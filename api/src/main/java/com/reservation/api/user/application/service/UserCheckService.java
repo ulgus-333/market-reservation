5834557851fa0d3065cfcd94e13b69bde3de7070
김지현 <ulgus@ulmac.local>
2025-06-15 14:55:58 +0900

    feature: #18 유저 아이디 찾기 검증 로직 구현

--- api/src/main/java/com/reservation/api/user/application/service/UserCheckService.java
+++ api/src/main/java/com/reservation/api/user/application/service/UserCheckService.java
@@ -1,5 +1,12 @@
 package com.reservation.api.user.application.service;
 
+import com.reservation.api.config.support.redis.RedisExecutor;
+import com.reservation.api.config.support.redis.RedisKey;
+import com.reservation.api.error.exception.BusinessException;
+import com.reservation.api.error.type.BadRequestType;
+import com.reservation.api.error.type.NotFoundType;
+import com.reservation.api.user.application.dto.IdVerificationDto;
+import com.reservation.api.user.presentation.dto.request.VerificationRequest;
 import com.reservation.api.user.presentation.dto.response.GenericResponse;
 import com.reservation.api.user.repository.UserEntityRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,11 +18,23 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UserCheckService {
     private final UserEntityRepository userEntityRepository;
+    private final RedisExecutor redisExecutor;
 
     public GenericResponse<Boolean> checkDuplicateId(String id) {
         Boolean duplicated = userEntityRepository.findById(id)
                 .isPresent();
 
         return GenericResponse.of(duplicated);
+    }
+
+    public GenericResponse<String> verificationCodeForFindId(VerificationRequest request) {
+        IdVerificationDto verificationDto = redisExecutor.findByKey(request.redisKey(RedisKey.USER_FIND_ID_KEY), IdVerificationDto.class)
+                .orElseThrow(() -> new BusinessException(NotFoundType.NOT_FOUND_VERIFICATION_DATA));
+
+        if (!verificationDto.equalsToken(request.verificationCode())) {
+            throw new BusinessException(BadRequestType.INVALID_VERIFICATION_CODE);
+        }
+
+        return GenericResponse.of(verificationDto.id());
     }
 }
---
+++ api/src/main/java/com/reservation/api/user/presentation/dto/request/VerificationRequest.java
@@ -0,0 +1,13 @@
+package com.reservation.api.user.presentation.dto.request;
+
+import com.reservation.api.config.support.redis.RedisKey;
+import jakarta.validation.constraints.NotBlank;
+
+public record VerificationRequest(
+        @NotBlank String uuid,
+        @NotBlank String verificationCode
+) {
+    public String redisKey(RedisKey redisKey) {
+        return redisKey.key(this.uuid);
+    }
+}
